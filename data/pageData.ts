9c5be300763609043ea857fb09f72a9ce84a2f3c
KsanaSal <oasalivon@gmail.com>
2023-06-02 15:28:33 +0300

    Added data file

---
+++ data/pageData.ts
@@ -0,0 +1,76 @@
+export const personalData = {
+    name: "Oksana Salivon",
+    role: "Front-end Developer",
+    education: [
+        "Faculty of Physical Engineering. Master of Science, National Technical University of Ukraine “Igor Sikorsky Kyiv Polytechnic Institute”(1997 - 2003)",
+    ],
+    AdditionalEducation: [
+        "Full Stack Developer, IT School GoIT (Kyiv)(2022 - 2023)",
+        "Front-end Developer, IT School SheCodes (online)(2022)",
+        "Web Design, IT School ITEA (Kyiv)(2019 - 2020)",
+    ],
+    contactLinks: [
+        "mailto:emailto:oasalivon@gmail.com",
+        "https://www.linkedin.com/in/oksana-salivon",
+        "https://github.com/KsanaSal",
+        "https://www.behance.net",
+    ],
+};
+
+export const aboutMe = {
+    title: "About Me",
+    body: [
+        "I am a passionate Front-end developer with a desire to learn and grow in a collaborative team environment. After working for 20 years in the Antonov company as a 3D engineer designer and had achieved maximum development in this field, I decided to completely change my field of activity. I am looking for an entry-level position with a company to use my skills in coding, troubleshooting complex problems, and assisting in the timely completion of projects. Skilled in HTML, CSS, JavaScript and React, Tailwind CSS, Node.js, Angular, I know English at an intermediate level and am learning it now to improve my level. Development, practice and work for the result are important for me",
+    ],
+};
+
+export const skills = {
+    soft: [
+        { icon: "👩‍🏫", text: "Attentive" },
+        { icon: "👥", text: "Collaboration" },
+        { icon: "👩‍💻", text: "Hardworking" },
+        { icon: "⽓", text: "Teamwork" },
+        { icon: "💡", text: "Creative Problem Solving" },
+        { icon: "⌛️", text: "Time management" },
+        { icon: "👩‍🎓", text: "Eager to learn" },
+        { icon: "🕵️‍♀️", text: "Meticulous" },
+    ],
+    hard: [
+        { icon: "💻", text: "JavaScript" },
+        { icon: "ʦ", text: "TypeScript" },
+        { icon: "⚛", text: "React" },
+        { icon: "💾", text: "SQL" },
+        { icon: "🐝", text: "Node.js" },
+        { icon: "A", text: "Angular" },
+        { icon: "F", text: "Figma" },
+        { icon: "Ai", text: "Illustrator" },
+        { icon: "Ps", text: "Photoshop" },
+        { icon: "💎", text: "AutoCad" },
+        { icon: "🌠JDS", text: "CATIA" },
+    ],
+};
+
+export const professionalData = {
+	title: "Professional Experience",
+	experiences: [
+		{
+			role: "Engineer designer 1 category (2017-Present)",
+			description:
+				"Development of external contours and theoretical foundations of aircraft, creation of blueprints of aircraft model parts for blowing.",
+			current: true,
+		},
+
+		{
+			role: "Engineer designer 2 category (2005-2017)",
+			description:
+				"Development of external contours and theoretical foundations of aircraft, creation of blueprints of aircraft model parts for blowing.",
+			current: false,
+		},
+		{
+			role: "Engineer designer 3 category (2002-2005)",
+			description:
+				"Development of external contours and theoretical foundations of aircraft, creation of blueprints of aircraft model parts for blowing.",
+			current: false,
+		},
+	],
+};
